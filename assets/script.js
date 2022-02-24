82bf15987fc893f60fae77a5c7be6c374f5eedda
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-02-23 20:34:13 -0700

    Added local storage properties to display recent searches.  Added part of UV index color coding.

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,6 @@ var today = moment().format('L');
 var currentCity = "";
 var mainDiv = $(".mainDiv");
 var searchHistory = $("#search-history");
-var historyArray = [];
 var current = $("#city-info");
 var fiveDay = $("#five-day");
 
@@ -11,29 +10,34 @@ var fiveDay = $("#five-day");
 
 const apiKey = 'ac785b3f1975916150e8a5fa906406d0'
 
+var historyArray = JSON.parse(localStorage.getItem("cityKey")) || [];
 
-
-// When the user clicks the search button, add the city to the 
-// search history list 
+for (i=0; i<historyArray.length; i++) {
+    var cityBtn = `<button id="btn${i}" value=${historyArray[i]}>${historyArray[i]}</button><br>`
+    searchHistory.append(cityBtn);
+    $(`#btn${i}`).click(function(event){
+        console.log(event.target);
+        var city = event.target.value;
+        currentConditions(city);
+    });
+}
 
 $(".submit").on("click", function(event) {
     event.preventDefault();
     var city = $(".cityInput").val();
-    // SET local storage here --- array of city names
-    localStorage.setItem("city", JSON.stringify(historyArray));
-    // IF the city is already in the array, don't push
-    
-    
-    // GET array/parse it from local storage
-    localStoarge.getItem()
-    // create a FOR loop that will create a button for each item in the array.
-    for (i=1; i<historyArray.length; i++) {
-        var cityBtn = $("<button>");
-        cityBtn.addClass("btn");
-        cityBtn.attr("type", "button");
-        cityBtn.attr("id", historyArray[i]);
+    if (historyArray.includes(city)){
+        return
+    }else{
+        historyArray.push(city)
+        var cityBtn = `<button value=${city}>${city}</button><br>`
         searchHistory.append(cityBtn);
     }
+    
+    // SET local storage here --- array of city names
+    localStorage.setItem("cityKey", JSON.stringify(historyArray));
+    // IF the city is already in the array, don't push
+   $(".cityInput").val('');
+
     currentConditions(city);
 });
 
@@ -72,33 +76,42 @@ function fetchData(lat, lon, city){
             var p1 = `<p>City:${city}</p>`
             var iconCode = `${data2.daily[0].weather[0].icon}`
             var iconUrl = `<img src= https://openweathermap.org/img/w/${iconCode}.png> `
+            
             var p2 = `<p>Temp:${data2.daily[0].temp.day}</p>`
             var p3 = `<p>Wind:${data2.daily[0].wind_speed}</p>`
             var p4 = `<p>Humidity:${data2.daily[0].humidity}</p>`
-            var p5 = `<p>UV Index:${data2.daily[0].uvi}</p>`
+            var p5 = `<p class="uvi">UV Index:${data2.daily[0].uvi}</p>`
             current.append(p1,today,iconUrl,p2,p3,p4,p5)
+            if (data2.daily[0].uvi < 5){
+                $(".uvi").css("background-color", "green")
+            }// }else if (data2.daily[0].uvi > ){
+
+            // }else{
+
+            // }
 
             for (i=1; i<6; i++){
+                var fourDaysForward = new moment().add(i, 'day');
                 var cardDiv = `
                 <div class="cardDiv">
+                    <p>${fourDaysForward.format('L')}</p>
                     <p>Temp:${data2.daily[i].temp.day}</p>
-
                     <p>Wind:${data2.daily[i].wind_speed}</p>
                     <p>Humidity:${data2.daily[i].humidity}</p>
-                    <p>UV Index:${data2.daily[i].uvi}</p>
+                    <p class="uvi${i}">UV Index:${data2.daily[i].uvi}</p>
                 </div>
                 `
                 fiveDay.append(cardDiv);
+
+                if (data2.daily[i].uvi < 5){
+                    $(`.uvi${i}`).css("background-color", "green")
+                }// }else if (data2.daily[i].uvi > ){
+    
+                // }else{
+                    
+                // }
             }
         })
 
     
 }
-
-
-
-
-
-
-//When I search for a city, I am presented with current and future conditions for that city.
-
